36effbea655ec669aa358790cfcd110d047ac3bc
Lucas Quintela Miranda <lucas.qmiranda@gmail.com>
2015-12-04 10:01:19 -0200

    Usuarios - index/sobre

--- user/index.php
+++ user/index.php
@@ -1,5 +1,87 @@
 <?php
-	require "header.php";
+require "header.php";
+?>
 
-	require "footer.php";
+<link href="vendor/bootstrap/css/business-frontpage.css" rel="stylesheet">
+
+<header class="business-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="tagline">ATS Development</h1>
+			</div>
+		</div>
+	</div>
+</header>
+
+<!-- Page Content -->
+<div class="container">
+
+	<hr>
+
+	<div class="row">
+		<div class="col-sm-8">
+			<h2>O que fazemos</h2>
+			<p>ATS Development é uma impresa com intuito de desemvolver softwares para agradar nossos cliente</p>
+			<p>Para fazer seu pedido é só de cadastrar</p>
+			<p>
+				<a class="btn btn-default btn-lg" href="cadastro.php">Cadastre-se &raquo;</a>
+			</p>
+		</div>
+		<div class="col-sm-4">
+			<h2>Contato</h2>
+			<address>
+				<strong>Lucas Quintela</strong><br>
+				<abbr title="Phone">Tel:</abbr>(27) 99741-2936
+				<br>
+				<abbr title="Email">E-mail:</abbr> <a href="mailto:lucas.qmiranda@gmail.com">lucas.qmiranda@gmail.com</a>
+			</address>
+			<address>
+				<strong>Carlos Souza</strong><br>
+				<abbr title="Phone">Tel:</abbr>(27) 99901-0796
+				<br>
+				<abbr title="Email">E-mail:</abbr> <a href="mailto:car.s.r.f@gmail.com">car.s.r.f@gmail.com</a>
+			</address>
+
+		</div>
+	</div>
+	<!-- /.row -->
+
+	<hr>
+
+	<div class="row">
+		<div class="col-sm-4">
+			<img class="img img-responsive img-center" src="img/pc.png" alt="">
+			<h2 align="center">Desktop</h2>
+			<!--p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p-->
+		</div>
+		<div class="col-sm-4">
+			<img class="img img-responsive img-center" src="img/web.png" alt="">
+			<h2 align="center">Web</h2>
+			<!--p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p-->
+		</div>
+		<div class="col-sm-4">
+			<img class="img img-responsive img-center" src="img/android.png" alt="">
+			<h2 align="center">Android</h2>
+			<!--p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p-->
+		</div>
+	</div>
+	<!-- /.row -->
+
+	<hr>
+
+	<!-- Footer -->
+	<footer>
+		<div class="row">
+			<div class="col-lg-12">
+				<p>Copyright &copy; ATSDevelopment 2015</p>
+			</div>
+		</div>
+		<!-- /.row -->
+	</footer>
+
+</div>
+
+<?php
+require "footer.php";
 ?>
---
+++ user/sobre.php
@@ -0,0 +1,61 @@
+<?php 
+require "header.php";
+?>
+<link href="css/round-about.css" rel="stylesheet">
+
+<div class="container">
+
+	<!-- Introduction Row -->
+	<div class="row">
+		<div class="col-lg-12">
+			<h1 class="page-header">Sobre
+				<small>ATS Development</small>
+			</h1>
+			<p>ATS Development é uma impresa com intuito de desemvolver softwares para agradar nossos cliente</p>
+		</div>
+	</div>
+
+	<!-- Team Members Row -->
+	<div class="row">
+		<div class="col-lg-12">
+			<h2 class="page-header">Time de Desenvolvedores</h2>
+		</div>
+		<div class="col-lg-4 col-sm-6 text-center">
+			<img class="img-circle img-responsive img-center" src="img/carlos.png" alt="">
+			<h3>Carlos Souza
+				<small>Desenvolvedor</small>
+			</h3>
+			
+		</div>
+		<div class="col-lg-4 col-sm-6 text-center">
+			<img class="img-circle img-responsive img-center" src="img/lucas.png" alt="">
+			<h3>Lucas Quintela
+				<small>Desenvolvedor</small>
+			</h3>
+		</div>
+		<div class="col-lg-4 col-sm-6 text-center">
+			<img class="img-circle img-responsive img-center" src="img/renan.png" alt="">
+			<h3>Renan Vago
+				<small>Designer</small>
+			</h3>
+		</div>
+	</div>
+
+	<hr>
+
+	<!-- Footer -->
+	<footer>
+		<div class="row">
+			<div class="col-lg-12">
+				<p>Copyright &copy; ATS Development 2015</p>
+			</div>
+			<!-- /.col-lg-12 -->
+		</div>
+		<!-- /.row -->
+	</footer>
+
+</div>
+
+<?php 
+require "footer.php";
+?>
